b7748e2c08794ccf5262a43319431d4fbfdecad5
Rajdeep Roy Chowdhury <rrajdeeproychowdhury@gmail.com>
2019-04-17 23:59:16 +0530

    File structure setup

---
+++ src/DoOrDieHandler.java
@@ -0,0 +1,31 @@
+import com.amazon.ask.Skill;
+import com.amazon.ask.Skills;
+import com.amazon.ask.SkillStreamHandler;
+import handlers.CancelandStopIntentHandler;
+import handlers.HelloWorldIntentHandler;
+import handlers.HelpIntentHandler;
+import handlers.SessionEndedRequestHandler;
+import handlers.LaunchRequestHandler;
+import handlers.FallbackIntentHandler;
+
+public class DoOrDieHandler extends SkillStreamHandler {
+
+    private static Skill getSkill() {
+        return Skills.standard()
+                .addRequestHandlers(
+                        new CancelandStopIntentHandler(),
+                        new HelloWorldIntentHandler(),
+                        new HelpIntentHandler(),
+                        new LaunchRequestHandler(),
+                        new SessionEndedRequestHandler(),
+                        new FallbackIntentHandler())
+                // Add your skill id below
+                .withSkillId("amzn1.ask.skill.4fba54b6-d53a-4fe5-8baf-e47cbc6550e1")
+                .build();
+    }
+
+    public DoOrDieHandler() {
+        super(getSkill());
+    }
+
+}
